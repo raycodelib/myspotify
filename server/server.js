9a6167b2faf697ed2d41e472ba739b2a9620784e
Ray <xielei8912@outlook.com>
2021-03-21 22:03:23 +1100

    1st server commit

---
+++ server/server.js
@@ -0,0 +1,67 @@
+const express = require("express");
+const cors = require("cors");
+// const bodyParser = require("body-parser");
+
+const spotifyWebApi = require("spotify-web-api-node");
+const lyricsFinder = require("lyrics-finder");
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+app.post("/refresh", (req, res) => {
+  const refreshToken = req.body.refreshToken;
+  const spotifyApi = new spotifyWebApi({
+    redirectUri: "http://localhost:3000",
+    clientId: "a83f1485c77f4e46826da5fa1f0aff86",
+    clientSecret: "d9555895f7524d7ba433ea0a15692522",
+    refreshToken: refreshToken,
+  });
+  spotifyApi
+    .refreshAccessToken()
+    .then((data) => {
+      console.log(data.body);
+      res.json({
+        accessToken: data.body.accessToken,
+        expiresIn: data.body.expiresIn,
+      });
+      //   // Save the access token so that it's used in future calls
+      //   spotifyApi.setAccessToken(data.body["access_token"]);
+    })
+    .catch((err) => {
+      console.log(err);
+      res.sendStatus(400);
+    });
+});
+
+app.post("/login", (req, res) => {
+  const code = req.body.code;
+  const spotifyApi = new spotifyWebApi({
+    redirectUri: "http://localhost:3000",
+    clientId: "a83f1485c77f4e46826da5fa1f0aff86",
+    clientSecret: "d9555895f7524d7ba433ea0a15692522",
+  });
+
+  spotifyApi
+    .authorizationCodeGrant(code)
+    .then((data) => {
+      res.json({
+        accessToken: data.body.access_token,
+        refreshToken: data.body.refresh_token,
+        expiresIn: data.body.expires_in,
+      });
+    })
+    .catch((err) => {
+      console.log(err);
+      res.sendStatus(400);
+    });
+});
+
+app.get("/lyrics", async (req, res) => {
+  const lyrics =
+    (await lyricsFinder(req.query.artist, req.query.track)) ||
+    "No Lyrics Found.";
+  res.json({ lyrics });
+});
+
+app.listen(3001);
